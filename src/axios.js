ef4b720f9664024ad2d9a846ec887b6d074be49e
TanveerAhmadRaj <ta78296@gmail.com>
2023-01-04 14:22:19 +0500

    Re Sync

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 //============================== BASE URL===========================
-axios.defaults.baseURL = "domain.par.clever-cloud.com/users/";
+axios.defaults.baseURL =
+  "app-dbd3cbd7-f92a-43ea-888c-15444348dd64.cleverapps.io/";
 axios.defaults.headers.common["Autherization"] =
   "Bearer " + localStorage.getItem("token");
