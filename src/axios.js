f7c78d4656aeec855d487acab71fa7732cc61fb8
TanveerAhmadRaj <ta78296@gmail.com>
2023-01-04 13:39:42 +0500

    Ok

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 //============================== BASE URL===========================
-axios.defaults.baseURL = "http://localhost:5000/users/";
+axios.defaults.baseURL = "domain.par.clever-cloud.com/users/";
 axios.defaults.headers.common["Autherization"] =
   "Bearer " + localStorage.getItem("token");
